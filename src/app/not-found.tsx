4eba37187bd3eff2ebda8257b6ce8a6c32d3b4cb
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-07-12 23:46:24 -0500

    🆕 Pagina 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { H1 } from "@/components/common/h1"
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-col min-h-dvh items-center justify-center py-10 px-4">
+      <H1>No se encontrá la página</H1>
+      <Link
+        href={"/catalogo"}
+        className="text-text-200 font-light cursor-pointer lg:hover:text-text-50 lg:transition-colors mt-5 text-lg lg:hover:underline"
+      >
+        Volver a la tienda
+      </Link>
+    </main>
+  )
+} 
